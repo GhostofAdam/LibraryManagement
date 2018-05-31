5bbb891b7e947ed1d6ddb34c91bedf51129112c9
GhostofAdam <gst0601@163.com>
2018-06-01 00:49:54 +0800

    Revert "createtable改动"
    This reverts commit feab371e4d2049181e260f8ac920458cfcb13697.

--- qt try/ui_outline/db.h
+++ qt try/ui_outline/db.h
@@ -3,7 +3,6 @@
 #include <QSqlDatabase>
 #include <logindialog.h>
 #include "configure.h"
-#include "data.h"
 class DB
 {
 private:
@@ -12,12 +11,6 @@ public:
     DB(const QString& path);
     ~DB();
     int EnterCheck(const QString& username,const QString& password);
-    bool CreateTable(int type);//创建成功和表已存在返回SUCCCESS=true，否则返回FAIL=false
-    bool DeleteTable(int type);//删除成功和表不存在返回SUCCCESS=true，否则返回FAIL=false
-    void PrintTablesType();//debug用，qdebug输出所有表类型，没几个表就没做查找了
-    bool isExist(const QString& key,const QString& value,const QString& table);//判断表table中是否有key列为value的行
-    //bool InsertUser(Data*data);//插入成功和用户已存在返回SUCCCESS=true，否则返回FAIL=false
-
 
 
 };
--- qt try/ui_outline/main.cpp
+++ qt try/ui_outline/main.cpp
@@ -4,7 +4,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     Controller Controller;
-    DB test("../../sqlite/admitDB.db");
-    test.CreateTable(USER_TYPE);
+
     return a.exec();
 }
