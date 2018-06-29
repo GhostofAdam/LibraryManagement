5970dca01c6449b2d91fd9fc9545092fbdc19178
GhostofAdam <gst0601@163.com>
2018-06-29 10:54:41 +0800

    searchreader

--- qt try/ui_outline/datarecord.h
+++ qt try/ui_outline/datarecord.h
@@ -29,18 +29,15 @@ public:
 
     virtual void Insert(QSqlDatabase& db);
     virtual void update(QSqlDatabase& db,QString key, QString value);
-<<<<<<< HEAD
-    virtual bool IsExist(QSqlDatabase& db){}
-    virtual void show(){
-        qDebug()<<recordID<<" "<<readerID<<" "<<bookID<<" "<<begintime<<" "<<endtime<<" "<<condition;
-    }
+
+
 
     Data* thebook(QSqlDatabase& db);
     Data* thereader(QSqlDatabase& db);
-=======
+
     virtual void show(){}
     virtual bool IsExist(QSqlDatabase& db){return 0;}
->>>>>>> 916793ca42385bd503d2f3f507e81f63df1dd620
+
 
 
 
